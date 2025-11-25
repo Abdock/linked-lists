3a4ca0c2fe50b4b5f207a396eceea726fe9e11ef
Yerzhigit Myrzabaev <e.mirzabaev@eskhata.com>
2025-11-26 00:59:25 +0500

    cleanup singly linked list

--- list/singly_linked_list.h
+++ list/singly_linked_list.h
@@ -2,16 +2,18 @@
 #define SINGLY_LINKED_LIST_H
 
 
-template <typename T>
+template<typename T>
 class SinglyLinkedList {
     struct Node {
         T data;
-        Node* next;
-        explicit Node(const T& val) : data(val), next(nullptr) {}
+        Node *next;
+
+        explicit Node(const T &val) : data(val), next(nullptr) {
+        }
     };
 
-    Node* head = nullptr;
-    Node* tail = nullptr;
+    Node *head = nullptr;
+    Node *tail = nullptr;
     size_t count = 0;
 
 public:
@@ -19,16 +21,17 @@ public:
 
     ~SinglyLinkedList() { clear(); }
 
-    SinglyLinkedList(const SinglyLinkedList&) = delete;
-    SinglyLinkedList& operator=(const SinglyLinkedList&) = delete;
+    SinglyLinkedList(const SinglyLinkedList &) = delete;
 
-    SinglyLinkedList(SinglyLinkedList&& other) noexcept
+    SinglyLinkedList &operator=(const SinglyLinkedList &) = delete;
+
+    SinglyLinkedList(SinglyLinkedList &&other) noexcept
         : head(other.head), tail(other.tail), count(other.count) {
         other.head = other.tail = nullptr;
         other.count = 0;
     }
 
-    SinglyLinkedList& operator=(SinglyLinkedList&& other) noexcept {
+    SinglyLinkedList &operator=(SinglyLinkedList &&other) noexcept {
         if (this != &other) {
             clear();
             head = other.head;
@@ -40,16 +43,16 @@ public:
         return *this;
     }
 
-    void push_front(const T& value) {
-        Node* node = new Node(value);
+    void push_front(const T &value) {
+        Node *node = new Node(value);
         node->next = head;
         head = node;
         if (!tail) tail = node;
         ++count;
     }
 
-    void push_back(const T& value) {
-        Node* node = new Node(value);
+    void push_back(const T &value) {
+        Node *node = new Node(value);
         if (tail) {
             tail->next = node;
         } else {
@@ -61,7 +64,7 @@ public:
 
     T pop_front() {
         if (!head) throw std::runtime_error("List is empty");
-        Node* node = head;
+        Node *node = head;
         T value = node->data;
         head = head->next;
         if (!head) tail = nullptr;
@@ -70,26 +73,26 @@ public:
         return value;
     }
 
-    T& front() {
+    T &front() {
         if (!head) throw std::runtime_error("List is empty");
         return head->data;
     }
 
-    T& back() {
+    T &back() {
         if (!tail) throw std::runtime_error("List is empty");
         return tail->data;
     }
 
-    bool contains(const T& value) const {
-        for (Node* curr = head; curr; curr = curr->next) {
+    bool contains(const T &value) const {
+        for (Node *curr = head; curr; curr = curr->next) {
             if (curr->data == value) return true;
         }
         return false;
     }
 
-    bool remove(const T& value) {
-        Node* prev = nullptr;
-        for (Node* curr = head; curr; prev = curr, curr = curr->next) {
+    bool remove(const T &value) {
+        Node *prev = nullptr;
+        for (Node *curr = head; curr; prev = curr, curr = curr->next) {
             if (curr->data == value) {
                 if (prev) prev->next = curr->next;
                 else head = curr->next;
@@ -104,7 +107,7 @@ public:
 
     void clear() {
         while (head) {
-            Node* tmp = head;
+            Node *tmp = head;
             head = head->next;
             delete tmp;
         }
@@ -116,12 +119,20 @@ public:
     bool empty() const { return count == 0; }
 
     class Iterator {
-        Node* curr;
+        Node *curr;
+
     public:
-        explicit Iterator(Node* n) : curr(n) {}
-        T& operator*() { return curr->data; }
-        Iterator& operator++() { curr = curr->next; return *this; }
-        bool operator!=(const Iterator& o) const { return curr != o.curr; }
+        explicit Iterator(Node *n) : curr(n) {
+        }
+
+        T &operator*() { return curr->data; }
+
+        Iterator &operator++() {
+            curr = curr->next;
+            return *this;
+        }
+
+        bool operator!=(const Iterator &o) const { return curr != o.curr; }
     };
 
     Iterator begin() { return Iterator(head); }
@@ -129,7 +140,7 @@ public:
 
     void print() const {
         std::cout << "[";
-        for (Node* curr = head; curr; curr = curr->next) {
+        for (Node *curr = head; curr; curr = curr->next) {
             std::cout << curr->data;
             if (curr->next) std::cout << ", ";
         }
